c8ee410b1e775e68b377db08353aa53373febdaa
Stanisław Horna <stanislawhorna@outlook.com>
2025-03-20 22:35:04 +0100

    redirect to login page if user is not logged in

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
-<script>
+<script lang="ts">
+	import { goto } from '$app/navigation';
 	import '../app.css';
+	import { get_user_details, getToken } from '$lib/api/auth';
+	import { onMount } from 'svelte';
+	
+	let is_user_authenticated = false;
+
+	onMount(async () => {
+		is_user_authenticated = false;
+		const token = getToken();
+		if (!token) {
+			goto("/login");
+		} else {
+			try {
+				await get_user_details();
+				is_user_authenticated = true;
+
+			} catch (e) {
+				console.error(e);
+				goto("/login");
+			}
+		}
+	});
 </script>
 
 <slot></slot>
