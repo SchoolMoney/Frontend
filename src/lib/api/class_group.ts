c86b223a0865e8bf3d181c187725a57b5edcfbe7
rukaSUKII <sobonukasz@gmail.com>
2025-04-06 13:21:05 +0200

    Change requests implementation to use middleware

--- src/lib/api/class_group.ts
+++ src/lib/api/class_group.ts
@@ -1,38 +1,20 @@
-import { getToken } from '$lib/api/auth';
-import { baseApiUrl } from '../../config';
+import { api_middleware } from "$lib/api_middleware"
+
 
 export async function get_user_classes_using_JWT() {
-	const response = await fetch(`${baseApiUrl}/api/class_group/list-class-groups`, {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			Authorization: `Bearer ${getToken()}`
-		}
-	});
-
-	if (response.ok) {
-		return await response.json();
-	} else {
-		const ErrorData = await response.json();
-		console.error(ErrorData['detail'] || `Error: ${response.status}`);
-		throw new Error(ErrorData['detail'] || `Error: ${response.status}`);
+	try {
+		return await api_middleware.get("/api/class_group/list-class-groups");
+	} catch (error){
+		console.error(error);
+		throw error;
 	}
 }
 
 export async function get_class_view(class_id: number){
-	const response = await fetch(`${baseApiUrl}/api/class_group/class-view?class_group_id=${class_id}`, {
-		method: 'GET',
-		headers: {
-			Authorization: `Bearer ${getToken()}`
-		}
-	});
-
-	if (response.ok) {
-		return await response.json();
-	}
-	else{
-		const ErrorData = await response.json();
-		console.error(ErrorData['detail'] || ErrorData['detail']['msg'] || `Error: ${response.status}`);
-		throw new Error(ErrorData['detail'] || ErrorData['detail']['msg'] || `Error: ${response.status}`);
+	try {
+		return await api_middleware.get(`/api/class_group/class-view?class_group_id=${class_id}`);
+	} catch (error){
+		console.error(error);
+		throw error;
 	}
 }
