8f54ae06d3b7b6a8b8c6b6c73de291cd6bb5483e
GringoXY <przemyslawkowalski01pk@gmail.com>
2025-04-27 16:02:45 +0200

    handle child update

--- src/routes/main/children/+page.svelte
+++ src/routes/main/children/+page.svelte
@@ -4,10 +4,10 @@
 	import { Card, CardDescription, CardHeader, CardTitle } from '$lib/components/ui/card';
 	import { goto } from '$app/navigation';
 	import { Alert, AlertDescription, AlertTitle } from '$lib/components/ui/alert';
-	import { CircleX, Plus } from 'lucide-svelte';
+	import { CircleX, Pencil, Plus, Trash } from 'lucide-svelte';
 	import { Button } from '$lib/components/ui/button';
 	import CardContent from '$lib/components/ui/card/card-content.svelte';
-	import type { AddChild, Child } from '$lib/models/child';
+	import type { AddChild, Child, UpdateChild } from '$lib/models/child';
 	import { addChild, deleteChild, getUserChildren, updateChild } from '$lib/api/child';
 	import type { ClassGroup } from '$lib/models/class_group';
 	import { Confirm } from '$lib/components/custom/confirm';
@@ -76,7 +76,7 @@ async function fetchClasses() {
 		goto(`classes/class-view?class_group_id=${class_id}`);
 	}
 
-  function getChildName(class_id: number): string {
+  function getClassName(class_id: number): string {
     return classes.find(c => c.id === class_id)?.name ?? '' ;
   }
 
@@ -120,9 +120,11 @@ async function fetchClasses() {
 
   async function handleChildSaveClick() {
     try {
-      await updateChild({
-        ...children.find(c => c.id === selectedChildId)!
-      });
+      const { name, surname, birth_date, group_id } = children.find(c => c.id === selectedChildId)!;
+      const childUpdateRequest: UpdateChild = {
+        name, surname, birth_date, group_id
+      }
+      await updateChild(selectedChildId, childUpdateRequest);
 
       selectedChildId = 0;
     } catch (error) {
@@ -131,7 +133,11 @@ async function fetchClasses() {
     }
   }
 
-  function handleChildCancelClick() {
+  function handleChildEditClick(child_id: number) {
+    selectedChildId = child_id;
+  }
+
+  function handleChildEditCancelClick() {
     selectedChildId = 0;
     fetchUserChildren();
   }
@@ -210,19 +216,73 @@ async function fetchClasses() {
           </form>
         {/if}
 				{#each children as child (child.id)}
-          <Card class="h-full transition-shadow hover:shadow-md">
-            <CardHeader>
-              <CardTitle>{child.name} {child.surname}</CardTitle>
-              <CardDescription>Birth date: {child.birth_date}<br/>Child: {getChildName(child.group_id)}</CardDescription>
-            </CardHeader>
-            <CardContent class="flex justify-end">
-              <Button
-                class="ms-auto"
-                on:click={() => handleClassDetailsClick(child.group_id)}>
-                Class details
-              </Button>
-            </CardContent>
-          </Card>
+          <form on:submit={handleChildSaveClick}>
+            <Card class="h-full transition-shadow hover:shadow-md">
+              {#if child.id === selectedChildId}
+                <CardHeader>
+                  <CardTitle class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <Input required bind:value={child.name} placeholder="Enter name" />
+                    <Input required bind:value={child.surname} placeholder="Enter surname" />
+                  </CardTitle>
+                  <CardDescription class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <Input required type="date" bind:value={child.birth_date} placeholder="Enter birth date" />
+                    <Select.Root
+                      selected={ { value: child.group_id, label: getClassName(child.group_id) }}
+                      onSelectedChange={(v) => {
+                        child.group_id = v?.value ?? 0;
+                      }}>
+                      <Select.Trigger>
+                        <Select.Value placeholder="Select class group" />
+                      </Select.Trigger>
+                      <Select.Content>
+                        {#each classes as classGroup}
+                          <Select.Item value={classGroup.id}>{classGroup.name}</Select.Item>
+                        {/each}
+                      </Select.Content>
+                    </Select.Root>
+                  </CardDescription>
+                </CardHeader>
+              {:else}
+                <CardHeader>
+                  <CardTitle>{child.name} {child.surname}</CardTitle>
+                  <CardDescription>Birth date: {child.birth_date}<br/>Child: {getClassName(child.group_id)}</CardDescription>
+                </CardHeader>
+              {/if}
+              <CardContent class="flex justify-between">
+                {#if child.id === selectedChildId}
+                  <div class="flex justify-end gap-2 w-full">
+                    <Button
+                      variant="secondary"
+                      on:click={handleChildEditCancelClick}>
+                      Cancel
+                    </Button>
+                    <Button
+                      type="submit"
+                      class="bg-green-600 text-white mt-auto hover:bg-opacity-85">
+                      Save
+                    </Button>
+                  </div>
+                {:else}
+                  <div>
+                    <Button
+                      class="bg-transparent text-green-600 hover:bg-green-600/10 p-2"
+                      on:click={() => handleChildEditClick(child.id)}>
+                      <Pencil class="scale-75" />
+                    </Button>
+                    <Button
+                      class="bg-transparent text-red-600 hover:bg-red-600/10 p-2"
+                      on:click={() => handleChildDeleteClick(child.id)}>
+                      <Trash class="scale-75" />
+                    </Button>
+                  </div>
+                  <Button
+                    on:click={() => handleClassDetailsClick(child.group_id)}>
+                    Details
+                  </Button>
+                {/if}
+              </CardContent>
+            </Card>
+          </form>
 				{/each}
 			</div>
 		</div>
