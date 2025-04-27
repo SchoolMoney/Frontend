1789c2e352aba939c6a093465a267a7ff62d1066
GringoXY <przemyslawkowalski01pk@gmail.com>
2025-04-27 15:29:32 +0200

    add creating childs on children view

--- src/routes/main/children/+page.svelte
+++ src/routes/main/children/+page.svelte
@@ -4,15 +4,18 @@
 	import { Card, CardDescription, CardHeader, CardTitle } from '$lib/components/ui/card';
 	import { goto } from '$app/navigation';
 	import { Alert, AlertDescription, AlertTitle } from '$lib/components/ui/alert';
-	import { CircleX } from 'lucide-svelte';
+	import { CircleX, Plus } from 'lucide-svelte';
 	import { Button } from '$lib/components/ui/button';
 	import CardContent from '$lib/components/ui/card/card-content.svelte';
-	import type { Child } from '$lib/models/child';
-	import { getUserChildren } from '$lib/api/child';
-	import type { ClassGroup } from '../../../lib/models/class_group';
-	import { getClasses } from '../../../lib/api/class_group';
+	import type { AddChild, Child } from '$lib/models/child';
+	import { addChild, deleteChild, getUserChildren, updateChild } from '$lib/api/child';
+	import type { ClassGroup } from '$lib/models/class_group';
+	import { Confirm } from '$lib/components/custom/confirm';
+	import { Input } from '$lib/components/ui/input';
+	import  * as Select from '$lib/components/ui/select';
+	import { getClasses } from '$lib/api/class_group';
 
-	const ERROR_DISPLAY_TIME = 3000;
+	const ERROR_DISPLAY_TIME = 10_000;
 
 	let errorMsg = '';
 	let showErrorPopup = false;
@@ -21,6 +24,18 @@
 	let children: Child[] = [];
   let classes: ClassGroup[] = [];
 	let isLoading = true;
+  let showAddingChild = false;
+  let addChildRequest: AddChild = {
+    name: '',
+    surname: '',
+    birth_date: '',
+    group_id: 0,
+  };
+
+  let selectedChildId = 0;
+  let selectedChildIdToDelete = 0;
+
+  let isConfirmChildDialogOpen = false;
 
 	async function fetchUserChildren() {
 		try {
@@ -28,25 +43,20 @@
 			if (!children?.length) {
 				errorMsg = 'No children found';
 			}
-
 		} catch (error) {
-			if (error instanceof Error) {
-				errorMsg = error.message;
-				showError();
-			}
+      errorMsg = error.message;
+      showError();
 		}
 	}
 
-  async function fetchClasses() {
-		try {
-			classes = await getClasses();
-		} catch (error) {
-			if (error instanceof Error) {
-				errorMsg = error.message;
-				showError();
-			}
-		}
+async function fetchClasses() {
+  try {
+    classes = await getClasses();
+  } catch (error) {
+    errorMsg = error.message;
+    showError();
   }
+}
 
 	function showError() {
 		if (errorTimeout) clearTimeout(errorTimeout);
@@ -62,19 +72,83 @@
 		isLoading = false;
 	})
 
-	function handleCardClick(class_id: number) {
+	function handleClassDetailsClick(class_id: number) {
 		goto(`classes/class-view?class_group_id=${class_id}`);
 	}
 
-  function getClassName(class_id: number): string {
+  function getChildName(class_id: number): string {
     return classes.find(c => c.id === class_id)?.name ?? '' ;
+  }
+
+  function handleAddChildClick() {
+    showAddingChild = true;
+  }
+
+  function handleCancelAddChildClick() {
+    showAddingChild = false;
+  }
+
+  async function handleAddChildSaveClick() {
+    try {
+      await addChild(addChildRequest);
+      addChildRequest = {
+        name: '',
+        surname: '',
+        birth_date: '',
+        group_id: 0,
+      };
+      showAddingChild = false;
+      await fetchUserChildren();
+    } catch (error) {
+      errorMessage = error.message;
+      showError();
+    }
+  }
+
+
+  async function confirmDeleteChild() {
+    try {
+      await deleteChild(selectedChildIdToDelete);
+      isConfirmChildDialogOpen = false;
+      children = children.filter(c => c.id !== selectedChildIdToDelete);
+      selectedChildIdToDelete = 0;
+    } catch (error) {
+      errorMessage = error.message;
+      showError();
+    }
+  }
+
+  async function handleChildSaveClick() {
+    try {
+      await updateChild({
+        ...children.find(c => c.id === selectedChildId)!
+      });
+
+      selectedChildId = 0;
+    } catch (error) {
+      errorMessage = error.message;
+      showError();
+    }
+  }
+
+  function handleChildCancelClick() {
+    selectedChildId = 0;
+    fetchUserChildren();
   }
 
 </script>
 
+<Confirm isOpen={isConfirmChildDialogOpen} onCancel={() => isConfirmChildDialogOpen = false} onConfirm={confirmDeleteChild} />
 
 <div class="min-h-dvh">
-  <h2 class="text-center text-4xl w-full font-bold">Children</h2>
+  <h2 class="text-center text-4xl w-full font-bold">
+    Children
+    <Button
+      class="bg-transparent hover:bg-green-600/10 text-green-600 mt-auto"
+      on:click={handleAddChildClick}>
+      <Plus class="h-4 w-4" />
+    </Button>
+  </h2>
 
 	{#if showErrorPopup}
 		<div class="fixed top-4 right-4 z-50 max-w-md" transition:fly={{ y: -30, duration: 300 }}>
@@ -95,16 +169,56 @@
 	{:else}
 		<div class="w-full overflow-y-auto max-h-[calc(100vh-150px)]">
 			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
+        {#if showAddingChild}
+          <form on:submit={handleAddChildSaveClick} class="grid gap-2">
+            <Card class="h-full transition-shadow hover:shadow-md">
+              <CardHeader>
+                <CardTitle class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <Input required bind:value={addChildRequest.name} placeholder="Enter name" />
+                  <Input required bind:value={addChildRequest.surname} placeholder="Enter surname" />
+                </CardTitle>
+                <CardDescription class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <Input required type="date" bind:value={addChildRequest.birth_date} placeholder="Enter birth date" />
+                  <Select.Root
+                    onSelectedChange={(v) => {
+                      addChildRequest.group_id = v?.value ?? 0;
+                    }}>
+                    <Select.Trigger>
+                      <Select.Value placeholder="Select class group" />
+                    </Select.Trigger>
+                    <Select.Content>
+                      {#each classes as classGroup}
+                        <Select.Item value={classGroup.id}>{classGroup.name}</Select.Item>
+                      {/each}
+                    </Select.Content>
+                  </Select.Root>
+                </CardDescription>
+              </CardHeader>
+              <CardContent class="flex justify-end gap-2">
+                <Button
+                  variant="secondary"
+                  on:click={handleCancelAddChildClick}>
+                  Cancel
+                </Button>
+                <Button
+                  type="submit"
+                  class="bg-green-600 text-white mt-auto hover:bg-opacity-85">
+                  Save
+                </Button>
+              </CardContent>
+            </Card>
+          </form>
+        {/if}
 				{#each children as child (child.id)}
           <Card class="h-full transition-shadow hover:shadow-md">
             <CardHeader>
               <CardTitle>{child.name} {child.surname}</CardTitle>
-              <CardDescription>Birth date: {child.birth_date}<br/>Class: {getClassName(child.group_id)}</CardDescription>
+              <CardDescription>Birth date: {child.birth_date}<br/>Child: {getChildName(child.group_id)}</CardDescription>
             </CardHeader>
             <CardContent class="flex justify-end">
               <Button
                 class="ms-auto"
-                on:click={() => handleCardClick(child.group_id)}>
+                on:click={() => handleClassDetailsClick(child.group_id)}>
                 Class details
               </Button>
             </CardContent>
