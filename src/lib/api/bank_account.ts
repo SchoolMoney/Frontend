b16b968c5e948b993ddd168ca5d5030f837b0559
rukaSUKII <sobonukasz@gmail.com>
2025-04-28 13:20:22 +0200

    Add request to backend regarding list of operations

--- src/lib/api/bank_account.ts
+++ src/lib/api/bank_account.ts
@@ -1,5 +1,5 @@
 import { api_middleware } from '../api_middleware';
-import type { BankAccount } from '../models/bank_account';
+import type { BankAccount, BankAccountOperation } from '../models/bank_account';
 
 export async function getBankAccountById(bank_account_id: number): Promise<BankAccount> {
 	try {
@@ -13,6 +13,15 @@ export async function getBankAccountById(bank_account_id: number): Promise<BankA
 export async function getUserBankAccount(): Promise<BankAccount> {
 	try {
 		return await api_middleware.get(`/api/bank_account/user`);
+	} catch (e) {
+		console.error(e);
+		throw e;
+	}
+}
+
+export async function getBankAccountOperations(bank_account_id: number): Promise<BankAccountOperation[]> {
+	try{
+		return await api_middleware.get(`/api/bank_account/operations/${bank_account_id}`);
 	} catch (e) {
 		console.error(e);
 		throw e;
