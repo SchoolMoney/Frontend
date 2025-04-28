20d65a634c7c609784dca5563575d95304a76ff7
rukaSUKII <sobonukasz@gmail.com>
2025-04-28 13:20:05 +0200

    Add model for bank operation

--- src/lib/models/bank_account.ts
+++ src/lib/models/bank_account.ts
@@ -4,3 +4,13 @@ export type BankAccount = {
   is_locked?: boolean;
   balance?: number;
 }
+
+export type BankAccountOperation = {
+  operation_id: number;
+  operation_date: string;
+  amount: number;
+  title: string;
+  description: string;
+  source_account_id?: number;
+  destination_account_id?: number;
+}
