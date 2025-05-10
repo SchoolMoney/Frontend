28e43197b7e1a9788ec6e82c8f71b6f28a671e86
GringoXY <przemyslawkowalski01pk@gmail.com>
2025-05-10 20:29:08 +0200

    add logo

--- src/routes/main/+layout.svelte
+++ src/routes/main/+layout.svelte
@@ -24,17 +24,28 @@
 		isAdmin = getSessionData().privilege === Privilege.ADMIN_USER;
 	});
 
-	const activeTabClasses: string = 'group bg-primary text-lg font-semibold text-primary-foreground md:text-base gap-2 hover:!text-background hover:bg-opacity-75 hover:text-opacity-75';
+	const activeTabClasses: string =
+		'group bg-primary text-lg font-semibold text-primary-foreground md:text-base gap-2 hover:!text-background hover:bg-opacity-75 hover:text-opacity-75';
 </script>
 
 <div class="flex w-full flex-col bg-muted/40">
 	<aside class="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
 		<nav class="flex flex-col items-center gap-4 px-2 sm:py-5">
+			<a
+				href="/main"
+				class="flex h-9 w-9 shrink-0 items-center justify-center overflow-hidden rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+			>
+				<img src="/logo.jpg" width={36} height={36} alt="Logo" />
+			</a>
 			<Tooltip.Root>
 				<Tooltip.Trigger asChild let:builder>
 					<a
 						href="/main/collections"
-						class="flex h-9 w-9 shrink-0 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {(page.url.pathname.includes('collections')) ? activeTabClasses : ''}"
+						class="flex h-9 w-9 shrink-0 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {page.url.pathname.includes(
+							'collections'
+						)
+							? activeTabClasses
+							: ''}"
 						use:builder.action
 						{...builder}
 					>
@@ -44,41 +55,53 @@
 				</Tooltip.Trigger>
 				<Tooltip.Content side="right">Collections</Tooltip.Content>
 			</Tooltip.Root>
-      {#if isAdmin === false}
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild let:builder>
-            <a
-              href="/main/classes"
-              class="flex h-9 w-9 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {(page.url.pathname.includes('classes')) ? activeTabClasses : ''}"
-              use:builder.action
-              {...builder}
-            >
-              <School class="h-5 w-5" />
-              <span class="sr-only">Classes</span>
-            </a>
-          </Tooltip.Trigger>
-          <Tooltip.Content side="right">Classes</Tooltip.Content>
-        </Tooltip.Root>
-        <Tooltip.Root>
-          <Tooltip.Trigger asChild let:builder>
-            <a
-              href="/main/children"
-              class="flex h-9 w-9 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {(page.url.pathname.includes('children')) ? activeTabClasses : ''}"
-              use:builder.action
-              {...builder}
-            >
-              <Baby class="h-5 w-5" />
-              <span class="sr-only">Children</span>
-            </a>
-          </Tooltip.Trigger>
-          <Tooltip.Content side="right">Children</Tooltip.Content>
-        </Tooltip.Root>
-      {/if}
+			{#if isAdmin === false}
+				<Tooltip.Root>
+					<Tooltip.Trigger asChild let:builder>
+						<a
+							href="/main/classes"
+							class="flex h-9 w-9 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {page.url.pathname.includes(
+								'classes'
+							)
+								? activeTabClasses
+								: ''}"
+							use:builder.action
+							{...builder}
+						>
+							<School class="h-5 w-5" />
+							<span class="sr-only">Classes</span>
+						</a>
+					</Tooltip.Trigger>
+					<Tooltip.Content side="right">Classes</Tooltip.Content>
+				</Tooltip.Root>
+				<Tooltip.Root>
+					<Tooltip.Trigger asChild let:builder>
+						<a
+							href="/main/children"
+							class="flex h-9 w-9 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {page.url.pathname.includes(
+								'children'
+							)
+								? activeTabClasses
+								: ''}"
+							use:builder.action
+							{...builder}
+						>
+							<Baby class="h-5 w-5" />
+							<span class="sr-only">Children</span>
+						</a>
+					</Tooltip.Trigger>
+					<Tooltip.Content side="right">Children</Tooltip.Content>
+				</Tooltip.Root>
+			{/if}
 			<Tooltip.Root>
 				<Tooltip.Trigger asChild let:builder>
 					<a
 						href="/main/reports"
-						class="flex h-9 w-9 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {(page.url.pathname.includes('reports')) ? activeTabClasses : ''}"
+						class="flex h-9 w-9 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {page.url.pathname.includes(
+							'reports'
+						)
+							? activeTabClasses
+							: ''}"
 						use:builder.action
 						{...builder}
 					>
@@ -92,7 +115,11 @@
 				<Tooltip.Trigger asChild let:builder>
 					<a
 						href="/main/chat"
-						class="flex h-9 w-9 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {(page.url.pathname.includes('chat')) ? activeTabClasses : ''}"
+						class="flex h-9 w-9 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {page.url.pathname.includes(
+							'chat'
+						)
+							? activeTabClasses
+							: ''}"
 						use:builder.action
 						{...builder}
 					>
@@ -109,7 +136,11 @@
 					<Tooltip.Trigger asChild let:builder>
 						<a
 							href="/main/admin"
-							class="flex h-9 w-9 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {(page.url.pathname.includes('admin')) ? activeTabClasses : ''}"
+							class="flex h-9 w-9 items-center justify-center rounded-full text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8 {page.url.pathname.includes(
+								'admin'
+							)
+								? activeTabClasses
+								: ''}"
 							use:builder.action
 							{...builder}
 						>
@@ -137,37 +168,57 @@
 					<nav class="grid gap-6 text-lg font-medium">
 						<a
 							href="/main/collections"
-							class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {(page.url.pathname.includes('collections')) ? activeTabClasses : ''}"
+							class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {page.url.pathname.includes(
+								'collections'
+							)
+								? activeTabClasses
+								: ''}"
 						>
 							<HandCoins class="h-5 w-5" />
 							Collections
 						</a>
-            {#if isAdmin === false}
-              <a
-                href="/main/classes"
-                class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {(page.url.pathname .includes('classes')) ? activeTabClasses : ''}"
-              >
-                <School class="h-5 w-5" />
-                Classes
-              </a>
-              <a
-                href="/main/children"
-                class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {(page.url.pathname.includes('children')) ? activeTabClasses : ''}"
-              >
-                <Baby class="h-5 w-5" />
-                Children
-              </a>
-            {/if}
+						{#if isAdmin === false}
+							<a
+								href="/main/classes"
+								class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {page.url.pathname.includes(
+									'classes'
+								)
+									? activeTabClasses
+									: ''}"
+							>
+								<School class="h-5 w-5" />
+								Classes
+							</a>
+							<a
+								href="/main/children"
+								class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {page.url.pathname.includes(
+									'children'
+								)
+									? activeTabClasses
+									: ''}"
+							>
+								<Baby class="h-5 w-5" />
+								Children
+							</a>
+						{/if}
 						<a
 							href="/main/reports"
-							class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {(page.url.pathname.includes('reports')) ? activeTabClasses : ''}"
+							class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {page.url.pathname.includes(
+								'reports'
+							)
+								? activeTabClasses
+								: ''}"
 						>
 							<Files class="h-5 w-5" />
 							Reports
 						</a>
 						<a
 							href="/main/chat"
-							class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {(page.url.pathname.includes('chat')) ? activeTabClasses : ''}"
+							class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {page.url.pathname.includes(
+								'chat'
+							)
+								? activeTabClasses
+								: ''}"
 						>
 							<MessageCircle class="h-5 w-5" />
 							Chat
@@ -175,7 +226,11 @@
 						{#if isAdmin}
 							<a
 								href="/main/admin"
-								class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {(page.url.pathname.includes('admin')) ? activeTabClasses : ''}"
+								class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground {page.url.pathname.includes(
+									'admin'
+								)
+									? activeTabClasses
+									: ''}"
 							>
 								<ShieldUser class="h-5 w-5" />
 								Admin panel
@@ -190,27 +245,31 @@
 					<Button
 						variant="outline"
 						size="icon"
-						class="overflow-hidden rounded-full {page.url.pathname.includes('account') ? 'border-2 border-primary' : ''}"
+						class="overflow-hidden rounded-full {page.url.pathname.includes('account')
+							? 'border-2 border-primary'
+							: ''}"
 						builders={[builder]}
 					>
-						<img
-							src="/src/images/logo.png"
-							width={36}
-							height={36}
-							alt="Avatar"
-						/>
+						<img src="/logo.jpg" width={36} height={36} alt="Avatar" />
 					</Button>
 				</DropdownMenu.Trigger>
 				<DropdownMenu.Content align="end">
-					<DropdownMenu.Item on:click={()=>{goto('/main/account')}}>Account</DropdownMenu.Item>
+					<DropdownMenu.Item
+						on:click={() => {
+							goto('/main/account');
+						}}>Account</DropdownMenu.Item
+					>
 					<DropdownMenu.Separator />
-					<DropdownMenu.Item on:click={()=>{logout(); goto('/login')}}>Logout</DropdownMenu.Item>
+					<DropdownMenu.Item
+						on:click={() => {
+							logout();
+							goto('/login');
+						}}>Logout</DropdownMenu.Item
+					>
 				</DropdownMenu.Content>
 			</DropdownMenu.Root>
 		</header>
-		<main
-			class="p-4 sm:px-6 sm:py-0"
-		>
+		<main class="p-4 sm:px-6 sm:py-0">
 			<slot></slot>
 		</main>
 	</div>
